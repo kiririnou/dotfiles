08e0133e1977ca256b4c6af44b29cc605aa29334
kiririnou <kou.pass94@gmail.com>
2024-10-21 12:17:00 +0300

    fix: updated nvim zenmode config

--- nvim/lua/kkll/lazy/zenmode.lua
+++ nvim/lua/kkll/lazy/zenmode.lua
@@ -4,7 +4,26 @@ return {
         vim.keymap.set("n", "<leader>zz", function()
             require("zen-mode").setup {
                 window = {
-                    width = 90,
+                    width = 120,
+                    options = {}
+                },
+                -- plugins = {
+                --     options = {
+                --         laststatus = 3,
+                --     },
+                -- },
+            }
+            require("zen-mode").toggle()
+            vim.wo.wrap = false
+            vim.wo.number = true
+            vim.wo.rnu = true
+            SetColor()
+        end)
+
+        vim.keymap.set("n", "<leader>zZ", function()
+            require("zen-mode").setup {
+                window = {
+                    width = 100,
                     options = {}
                 },
             }
@@ -15,7 +34,7 @@ return {
             SetColor()
         end)
 
-        vim.keymap.set("n", "<leader>zZ", function()
+        vim.keymap.set("n", "<leader>ZZ", function()
             require("zen-mode").setup {
                 window = {
                     width = 80,
@@ -31,3 +50,4 @@ return {
         end)
     end
 }
+
