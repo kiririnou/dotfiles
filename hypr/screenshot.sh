56f2fd14c4381f1199a85882b43ab72451d2706c
kiririnou <kou.pass94@gmail.com>
2025-03-03 17:59:06 +0200

    hypr: updated screenshot.sh

--- hypr/screenshot.sh
+++ hypr/screenshot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 grim -g "$(slurp -b 1B1F28CC -c E06B74ff -s C778DD0D -w 2)" - | \
-satty --filename - --output-filename ~/Pictures/Screenshots/Screenshot_$(date '+%Y%m%d-%H:%M:%S').png --init-tool pointer --copy-command wl-copy
+satty --filename - --output-filename ~/Pictures/Screenshots/Screenshot_$(date '+%Y%m%d-%H:%M:%S').png --init-tool pointer --copy-command wl-copy --early-exit
