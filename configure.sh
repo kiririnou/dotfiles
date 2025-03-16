8058d6ebd980db3fef9aab099f2a532d5013d644
kiririnou <kou.pass94@gmail.com>
2025-03-16 19:20:34 +0200

    dotfiles: configure.sh rewrite

--- configure.sh
+++ configure.sh
@@ -3,38 +3,128 @@
 # set -x
 
 dotfiles_dir=$(pwd)
-
-#create backups of old config
+backup_prefix="$dotfiles_dir/backup"
 datetime=$(date '+%Y%m%d-%H_%M_%S')
+config_list=("nvim" "ghostty" "scripts" "hypr" "tmux")
+force_install=0
 
-mkdir backup_$datetime
-cp $HOME/.tmux.conf $dotfiles_dir/backup/.tmux.conf_$datetime.bak
-cp -r $HOME/.config/nvim $dotfiles_dir/backup/nvim_$datetime.bak
-cp -r $HOME/.config/ghostty $dotfiles_dir/backup/ghostty_$datetime.bak
-cp -r $HOME/.local/scripts $dotfiles_dir/backup/scripts_$datetime.bak
-cp -r $HOME/.config/hypr $dotfiles_dir/backup/hypr_$datetime.bak
+install_path() {
+    case "$1" in
+        "tmux")
+            echo "$HOME/.tmux.conf"
+            ;;
+        "scripts")
+            echo "$HOME/.local/scripts"
+            ;;
+        *)
+            echo "$HOME/.config/$1"
+            ;;
+    esac
+}
 
-rm $HOME/.tmux.conf
-cp $dotfiles_dir/tmux/.tmux.conf $HOME/.tmux.conf
+create_backup() {
+    backup_path="$backup_prefix/backup_$datetime"
+    mkdir -p $backup_path
 
-rm -fr $HOME/.config/nvim
-cp -r $dotfiles_dir/nvim $HOME/.config/nvim
+    for config in "${config_list[@]}"; do
+        cp -r $(install_path $config) $backup_path/$config.bak 
+    done
 
-rm -fr $HOME/.config/ghostty
-cp -r $dotfiles_dir/ghostty $HOME/.config/ghostty
+    echo "Created backup in $backup_path"
+}
 
-rm -r $HOME/.local/scripts
-cp -r $dotfiles_dir/scripts $HOME/.local/scripts
+compare_configs() {
+    local status=0
 
-rm -r $HOME/.config/hypr
-cp -r $dotfiles_dir/hypr $HOME/.config/hypr
+    for config in "${config_list[@]}"; do
+        diff -ur $(install_path $config) $dotfiles_dir/$config
+        status="$?"
 
-bash_ext="$dotfiles_dir/bash_ext.sh"
-if ! grep -Fq $bash_ext $HOME/.bashrc ; then
-    echo "bash_ext.sh is not included in .bashrc. adding to .bashrc..."
-    echo ". \"$bash_ext\"" >> $HOME/.bashrc
-else
-    echo "bash_ext.sh is already included in .bashrc. skip..."
-fi
+        if [ "$status" -ne 0 ]; then
+            echo "Local configs were modified."
+            echo "Please check if no important modifications are lost."
+            [ "$force_install" -eq 0 ] && exit $status
+        fi
+    done
+}
 
+# check if request config exists in $dotfiles_dir
+check_config_existence() {
+    local path="$dotfiles_dir/$1"
+    if [ -d "$path" ]  || [ -e "$path" ]; then
+        echo ">$path config exists. all good"
+        return 1
+    else
+        echo ">$path doesn't exists"
+        return 0
+    fi
+}
+
+# argument for specific config
+install_config() {
+    check_config_existence $1
+    file_state=$?
+
+    if [ "$file_state" -ne 0 ]; then
+        echo ">instaling config for $1"
+        rm -fr $(install_path $1)
+        cp -r $dotfiles_dir/$1 $(install_path $1)
+        echo ">$1 config installed"
+    fi
+}
+
+install_bash_ext() {
+    bash_ext="$dotfiles_dir/bash_ext.sh"
+    if ! grep -Fq $bash_ext $HOME/.bashrc ; then
+        echo "bash_ext.sh is not included in .bashrc. adding to .bashrc..."
+        echo ". \"$bash_ext\"" >> $HOME/.bashrc
+    else
+        echo "bash_ext.sh is already included in .bashrc. skip..."
+    fi
+}
+
+parse_opts() {
+    while getopts "fc" options; do
+        case "${options}" in
+            f)
+                force_install=1
+                echo "Force install is enabled. Ignoring local modifications..."
+                ;;
+            c)
+                compare_configs
+                exit 0
+                ;;
+            :)
+                echo "Missing arguments"
+                exit 1
+                ;;
+            *)
+                echo "Unknown argument"
+                exit 1
+                ;;
+        esac
+    done
+}
+
+run() {
+    parse_opts "$@"
+    # shift arguments to skip what processed by getopts
+    shift $((OPTIND - 1))
+
+    create_backup
+    compare_configs
+
+    if [ "$#" -gt 0 ]; then
+        unset config_list
+        IFS=" " read -ra config_list <<< "$@"
+    fi
+    
+    for config in "${config_list[@]}"; do
+        install_config $config
+    done
+
+    install_bash_ext
+}
+
+run "$@"
 echo "Done."
